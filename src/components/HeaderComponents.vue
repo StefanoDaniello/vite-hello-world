d670e8ad396b7d87b9c0c9924f0124cab908fe67
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-09 16:40:57 +0200

    add title

---
+++ src/components/HeaderComponents.vue
@@ -0,0 +1,22 @@
+<template>
+   <header>
+      <h1>{{ title }}</h1>
+   </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponents',
+        components: {
+        },
+        data() {
+          return {
+            title: 'La mia prima app con Vite!'
+          }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
